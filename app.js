3d57ce47f51533cc2c71ae1367a9ecf669084a80
M Thariq Nugroho <thariq1902@gmail.com>
2020-05-27 16:39:26 +0700

    initial

---
+++ app.js
@@ -0,0 +1,61 @@
+var stompClient = null;
+
+var url = "http://147.139.164.106:8199";
+var captured = "188"; // Value is in [1] ('384' in our case)
+var result = captured ? captured : 'defaultValue';
+
+console.log('extension_user = ' + result);
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS(`${url}/gs-guide-websocket`);
+    console.log({socket})
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting) {
+            showGreeting(JSON.parse(greeting.body).content);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+}
+
+function sendStatus() {
+    stompClient.send("/app/hello", {}, JSON.stringify({'name': result}));
+}
+
+function showGreeting(message) {
+    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendName(); });
+});
