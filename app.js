2d6dd8bd4a51ed588589d3034ed6b1d42f841c25
M Thariq Nugroho <thariq1902@gmail.com>
2020-05-29 12:47:08 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,61 +1,76 @@
 var stompClient = null;
 
-var url = "http://147.139.164.106:8199";
-var captured = "188"; // Value is in [1] ('384' in our case)
-var result = captured ? captured : 'defaultValue';
+var url = "https://iswitch.ddns.net:8199";
+var captured = "199";
+var result = captured ? captured : "defaultValue";
 
-console.log('extension_user = ' + result);
+console.log("extension_user = " + result);
 
 function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
+  $("#connect").prop("disabled", connected);
+  $("#disconnect").prop("disabled", !connected);
+  if (connected) {
+    $("#conversation").show();
+  } else {
+    $("#conversation").hide();
+  }
+  $("#greetings").html("");
 }
 
 function connect() {
-    var socket = new SockJS(`${url}/gs-guide-websocket`);
-    console.log({socket})
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/greetings', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
-        });
-    });
-}
+  var socket = new SockJS(`${url}/gs-guide-websocket`);
+  stompClient = Stomp.over(socket);
+  console.log({ stompClient, socket });
+  stompClient.connect({}, function (frame) {
+    setConnected(true);
+    console.log("Connected: " + frame);
 
-function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
+    stompClient.subscribe("/topic/acw/199", (greeting) => {
+      const res = JSON.parse(greeting.body);
+      console.log({ res });
+      showGreeting(res.detail);
+    });
+  });
 }
 
 function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+  stompClient.send(
+    `/app/acw/${captured}`,
+    {},
+    JSON.stringify({
+      extension: captured,
+      cases: "3",
+      detail: "ini detail 3",
+    })
+  );
 }
 
+function disconnect() {
+  if (stompClient !== null) {
+    stompClient.disconnect();
+  }
+  setConnected(false);
+  console.log("Disconnected");
+}
 function sendStatus() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': result}));
+  stompClient.send("/app/hello", {}, JSON.stringify({ name: result }));
 }
 
 function showGreeting(message) {
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
+  $("#greetings").append(`<tr><td> ${message} <tr><td>`);
 }
 
 $(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+  });
+  $("#connect").click(function () {
+    connect();
+  });
+  $("#disconnect").click(function () {
+    disconnect();
+  });
+  $("#send").click(function () {
+    sendName();
+  });
 });
